55b1648f35e59b8c8eb72e42f7327e7a5f6a7464
D Harris <68394263+D-Harr@users.noreply.github.com>
2024-11-05 17:38:28 -0500

    Third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,20 @@
-// Create web server 
+// Create web server with express
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+const comments = require('./comments');
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+app.post('/comments', (req, res) => {
+  comments.push(req.body);
+  res.status(201).json(req.body);
+});
+app.listen(3000);
+// comments.js
+module.exports = [
+  {
+        id: 1,
+      }
+    ];
